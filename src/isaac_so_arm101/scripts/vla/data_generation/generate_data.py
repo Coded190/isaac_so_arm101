807bc482fd1e52610ced76bb80ebd32835782b5d
Coded190 <moorechristian15@yahoo.com>
2026-05-17 18:25:05 -1000

    increase hdri intensity

--- src/isaac_so_arm101/scripts/vla/data_generation/generate_data.py
+++ src/isaac_so_arm101/scripts/vla/data_generation/generate_data.py
@@ -284,7 +284,7 @@ def randomize_lighting(stage, hdri_folder_path, env_ids=None):
 
     chosen_hdri = random.choice(hdri_files)
     full_path = os.path.join(hdri_folder_path, chosen_hdri)
-    intensity = random.uniform(1.0, 2.0)
+    intensity = random.uniform(500.0, 1500.0)
 
     if env_ids is None:
         env_ids = []
