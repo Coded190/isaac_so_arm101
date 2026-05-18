999a703e0e06927b3624e8c54758582fb9cfbda4
Coded190 <moorechristian15@yahoo.com>
2026-05-17 22:18:42 -1000

    change to 50 intensity

--- src/isaac_so_arm101/scripts/vla/data_generation/generate_data.py
+++ src/isaac_so_arm101/scripts/vla/data_generation/generate_data.py
@@ -339,9 +339,9 @@ def randomize_lighting(stage, hdri_folder_path, env_ids=None):
         if env_id == 0:
             light.GetIntensityAttr().Set(target_intensity)
         else:
-            # 400.0 is dim enough to not ruin the exposure, but high enough 
+            # value set is dim enough to not ruin the exposure, but high enough 
             # to keep the HDRI visible in the local cameras!
-            light.GetIntensityAttr().Set(400.0)
+            light.GetIntensityAttr().Set(50.0)
         
         # (Optional but highly recommended) Force latlong format to prevent viewport blackouts
         format_attr = light.GetTextureFormatAttr()
